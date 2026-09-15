71bacdfaa084a83d22fbf9efcca3504221666af0
Mehrshad <mehrshad.dm@yahoo.com>
2026-09-15 16:58:09 +0330

    this is a first changed for develop this mini app

--- work_with_lists.py
+++ work_with_lists.py
@@ -1,4 +1,7 @@
-my_list = [1, 2, 3, 4, 5]
-my_list.remove(3)
-my_list.append(10)
-print(my_list)
+my_list = [1, 2, 3, 4, 5, "a"]
+for this_item in my_list:
+    print(this_item)
+    print(f"*2 = {this_item*2}")
+print("end the loop")
+if this_item == "a" :
+    print("wowww")
